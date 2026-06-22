bdb624568a356c92fb903fb83c92c9a3d4e55a6b
christemami-tech <christemami@gmail.com>
2026-06-22 16:35:19 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-CHANNELS = ["@DrDoctorMeme","@TheBoysinTelegram","@drtel"]
+CHANNELS = ["@DrDoctorMeme","@TheBoysinTelegram"]
 
 movies = {
     "film1": "BAACAgQAAxkBAAMIajkh_DWv2QFg47JFNg1uv9x8LMoAApUcAAKn6sFRm5aI2hDZHmw8BA"
